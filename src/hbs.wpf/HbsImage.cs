488872082d684db85b30072e0f035acbb9f493ef
Marcus Specht <m.specht@picibird.com>
2018-09-14 02:44:04 +0200

    cleanup and HbsImage to control render performance quality

---
+++ src/hbs.wpf/HbsImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace System.Windows.Controls
+{
+    public class HbsImage : Image
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            this.VisualBitmapScalingMode = BitmapScalingMode.NearestNeighbor;
+            base.OnRender(dc);
+        }
+    }
+}
